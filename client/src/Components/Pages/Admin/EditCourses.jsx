e68bc0bcf60d92cc2c0819e5354bb1238ec93695
Danish <dan71ish@gmail.com>
2024-04-02 00:35:18 +0530

    just add the backend route in the deployed version

--- client/src/Components/Pages/Admin/EditCourses.jsx
+++ client/src/Components/Pages/Admin/EditCourses.jsx
@@ -5,16 +5,16 @@ function TopBar() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!localStorage.getItem('admin_token')) {
-      navigate('/register-admin')
+    if (!localStorage.getItem("admin_token")) {
+      navigate("/register-admin");
     }
-  }, ['admin_token'])
+  }, ["admin_token"]);
 
   function addCourse() {
-    if (!localStorage.getItem('admin_token')) {
-      navigate("/register-admin")
+    if (!localStorage.getItem("admin_token")) {
+      navigate("/register-admin");
     } else {
-      navigate('/editcourses-add')
+      navigate("/editcourses-add");
     }
   }
 
@@ -22,68 +22,91 @@ function TopBar() {
     <div className="fixed w-full">
       <div className="bg-custom-blue font-sans flex border border-custom-light">
         <div className="flex items-center p-3 inline-flex">
-          <img src="/logo.png" className="w-10 h-10 rounded-3xl" alt="logo"></img>
+          <img
+            src="/logo.png"
+            className="w-10 h-10 rounded-3xl"
+            alt="logo"
+          ></img>
           <div className="ml-1 text-white text-xl font-bold">HyperDev</div>
         </div>
         <div className="flex justify-end flex-grow inline-flex m-4">
           <div className="ml-1 p-1 border border-custom-light rounded-xl text-white text-xs md:text-sm">
-            <button onClick={addCourse}>Add Courses</button></div>
+            <button onClick={addCourse}>Add Courses</button>
+          </div>
           <div className="ml-1 p-1 border border-custom-light rounded-xl text-white text-xs bg-blue-700 md:text-sm">
-            <button onClick={() => {
-              localStorage.removeItem('admin_token')
-              navigate("/")
-            }}>Logout</button></div>
+            <button
+              onClick={() => {
+                localStorage.removeItem("admin_token");
+                navigate("/");
+              }}
+            >
+              Logout
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
-function CoursesCard(props){
-  const navigate = useNavigate('/');
+function CoursesCard(props) {
+  const navigate = useNavigate("/");
 
-  function deleteCourse(){
-    fetch('http://localhost:3000/admin/editcourses/delete',{
-      method:'DELETE',
-      body:JSON.stringify({
-        id: props.course._id
+  function deleteCourse() {
+    fetch(`{import.meta.env.VITE_SERVER_LOCATION}/admin/editcourses/delete`, {
+      method: "DELETE",
+      body: JSON.stringify({
+        id: props.course._id,
       }),
-      headers:{
-        "Content-type":"application/json",
-        "Authorization":`Bearer ${localStorage.getItem('admin_token')}`
-    }
-  })
-  .then(async (res)=>{
-    const json = await res.json();
-    alert("Course deleted")
-    navigate("/editcourses")
-  })
- }
- function updateCourse(){
-  navigate('/editcourses-update')
- }
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("admin_token")}`,
+      },
+    }).then(async (res) => {
+      const json = await res.json();
+      alert("Course deleted");
+      navigate("/editcourses");
+    });
+  }
+  function updateCourse() {
+    navigate("/editcourses-update");
+  }
 
-    const { course } = props; // Destructure course from props
-    return (
-        <div className="border border-blue-900 rounded-xl p-4 ">
-            <img src={course.imageLink} className="w-60 h-36 rounded-xl" alt={course.title} />
-            <div>
-                <div className="font-bold text-lg">{course.title}</div>
-                <div className="text-gray-600">{course.description}</div>
-                <div className="text-blue-900 font-bold">{course.price}</div>
-                  <button onClick={updateCourse} className="mt-2 bg-blue-900 text-white px-4 py-2 rounded-md hover:bg-blue-700">Edit</button>
-                  <button onClick={deleteCourse} className="mt-2 ml-8 bg-blue-900 text-white px-4 py-2 rounded-md hover:bg-blue-700">Delete</button>
-            </div>
-        </div>
-    );
+  const { course } = props; // Destructure course from props
+  return (
+    <div className="border border-blue-900 rounded-xl p-4 ">
+      <img
+        src={course.imageLink}
+        className="w-60 h-36 rounded-xl"
+        alt={course.title}
+      />
+      <div>
+        <div className="font-bold text-lg">{course.title}</div>
+        <div className="text-gray-600">{course.description}</div>
+        <div className="text-blue-900 font-bold">{course.price}</div>
+        <button
+          onClick={updateCourse}
+          className="mt-2 bg-blue-900 text-white px-4 py-2 rounded-md hover:bg-blue-700"
+        >
+          Edit
+        </button>
+        <button
+          onClick={deleteCourse}
+          className="mt-2 ml-8 bg-blue-900 text-white px-4 py-2 rounded-md hover:bg-blue-700"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 }
 export function EditCourses() {
   const [courses, setCourses] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/user/courses')
-      .then(response => response.json())
-      .then(data => setCourses(data.courses))
-      .catch(error => console.error("Error while fetching:", error));
+    fetch(`${import.meta.env.VITE_SERVER_LOCATION}/user/courses`)
+      .then((response) => response.json())
+      .then((data) => setCourses(data.courses))
+      .catch((error) => console.error("Error while fetching:", error));
   }, []);
 
   return (
@@ -100,10 +123,11 @@ export function EditCourses() {
         </div>
       ) : (
         <h2 className="pt-24 flex flex-row items-center justify-center text-black">
-          {courses ? "Oops! No course is currently offered. Return later!" : "Loading..."}
+          {courses
+            ? "Oops! No course is currently offered. Return later!"
+            : "Loading..."}
         </h2>
       )}
-
     </div>
   );
 }
--- client/src/Components/Pages/User/UserLogin.jsx
+++ client/src/Components/Pages/User/UserLogin.jsx
@@ -1,77 +1,93 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export function UserLogin() { 
-  const[email,setEmail]=useState('')
-  const[password,setPassword]=useState('')
-  const navigate = useNavigate('/')
+export function UserLogin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate("/");
   return (
-    <div className="bg-custom-blue flex flex-col h-screen items-center justify-center"> 
-    <div className="bg-white p-8 text-black text-lg border border-blue-900 rounded-2xl ">
-
-    <div className="text-center font-medium text-custom-blue text-2xl">
-    Welcome to HyperDev </div> 
-
-    <div className="p-2">User</div>
-
-      <input className="p-2 rounded-xl text-black border border-black" type="email" placeholder="Email" onChange={(e)=>{
-        const value = e.target.value;
-        setEmail(value);
-      }}/>  <br/> <br />
-
-      <input className="p-2 rounded-xl text-black border border-black" type="password" placeholder="Password" onChange={(e)=>{
-        const value = e.target.value;
-        setPassword(value);
-      }}/> <br /> <br />
-
-      <div className="text-center p-2 border border-custom-light rounded-xl text-white text-lg bg-blue-700 md:text-base">
-      <button onClick={()=>{
-        fetch('http://localhost:3000/user/signin',{
-          method:"POST",
-          body:JSON.stringify({
-            email:email,
-            password:password
-          }),
-          headers:{
-            "Content-Type":"application/json"
-          }
-        })
-        .then(response => response.json())
-          .then(data => {
-            if (data.success) {
-              const token = data.token;
-              localStorage.setItem('token',token);
-              console.log('User Token:',token);
-              navigate('/purchasedcourses')
-              } else {
-                alert("Incorrect username or password"); // Show failure message
-            }
-})}}> Login</button> </div> <br />
-
-      <div className="pl-2">
-        Don't have an account? Signup
+    <div className="bg-custom-blue flex flex-col h-screen items-center justify-center">
+      <div className="bg-white p-8 text-black text-lg border border-blue-900 rounded-2xl ">
+        <div className="text-center font-medium text-custom-blue text-2xl">
+          Welcome to HyperDev{" "}
+        </div>
+        <div className="p-2">User</div>
+        <input
+          className="p-2 rounded-xl text-black border border-black"
+          type="email"
+          placeholder="Email"
+          onChange={(e) => {
+            const value = e.target.value;
+            setEmail(value);
+          }}
+        />{" "}
+        <br /> <br />
+        <input
+          className="p-2 rounded-xl text-black border border-black"
+          type="password"
+          placeholder="Password"
+          onChange={(e) => {
+            const value = e.target.value;
+            setPassword(value);
+          }}
+        />{" "}
+        <br /> <br />
+        <div className="text-center p-2 border border-custom-light rounded-xl text-white text-lg bg-blue-700 md:text-base">
+          <button
+            onClick={() => {
+              fetch(`${import.meta.env.VITE_SERVER_LOCATION}/user/signin`, {
+                method: "POST",
+                body: JSON.stringify({
+                  email: email,
+                  password: password,
+                }),
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              })
+                .then((response) => response.json())
+                .then((data) => {
+                  if (data.success) {
+                    const token = data.token;
+                    localStorage.setItem("token", token);
+                    console.log("User Token:", token);
+                    navigate("/purchasedcourses");
+                  } else {
+                    alert("Incorrect username or password"); // Show failure message
+                  }
+                });
+            }}
+          >
+            {" "}
+            Login
+          </button>{" "}
+        </div>{" "}
+        <br />
+        <div className="pl-2">Don't have an account? Signup</div>
+        <br />
+        <div className="text-center p-2 border border-custom-light rounded-xl text-white text-lg bg-blue-700 md:text-base">
+          <button
+            onClick={() => {
+              fetch(`${import.meta.env.VITE_SERVER_LOCATION}/user/signup`, {
+                method: "POST",
+                body: JSON.stringify({
+                  email: email,
+                  password: password,
+                }),
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }).then(async (res) => {
+                const json = await res.json();
+                alert("user added");
+              });
+            }}
+          >
+            Sign Up
+          </button>{" "}
+        </div>{" "}
+        <br />
       </div>
-      <br />
-      <div className="text-center p-2 border border-custom-light rounded-xl text-white text-lg bg-blue-700 md:text-base">
-      <button onClick={()=>{
-        fetch('http://localhost:3000/user/signup',{
-          method:"POST",
-          body:JSON.stringify({
-            email:email,
-            password:password
-          }),
-          headers:{
-            "Content-Type":"application/json"
-          }
-        })
-        .then(async (res)=>{
-          const json = await res.json();
-          alert("user added")
-
-      })
-      }}>Sign Up</button> </div> <br /> 
-
-    </div>
     </div>
   );
 }
